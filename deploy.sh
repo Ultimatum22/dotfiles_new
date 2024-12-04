7bbce1a10c35d15be4fd74346039047d8c127eb5
Dave N <4242374+Ultimatum22@users.noreply.github.com>
2024-12-04 08:03:43 +0100

    Initial structure

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+
+set -e
+
+# Get the current path
+SCRIPT_DIR="${0:A:h}"
+cd "${SCRIPT_DIR}"
+
+# Default XDG paths
+XDG_CACHE_HOME="${HOME}/.cache"
+XDG_CONFIG_HOME="${HOME}/.config"
+XDG_DATA_HOME="${HOME}/.local/share"
+XDG_STATE_HOME="${HOME}/.local/state"
+
+# Create required directories
+print "Creating required directories..."
+mkdir -p "${XDG_CONFIG_HOME}"/{git/local,mc,htop,tig,gnupg,nvim}
+mkdir -p "${XDG_CACHE_HOME}"/{vim/{backup,swap,undo},zsh,tig}
+mkdir -p "${XDG_DATA_HOME}"/{{goenv,jenv,luaenv,nodenv,phpenv,plenv,pyenv,rbenv}/plugins,zsh,man/man1,vim/spell,nvim/site/pack/plugins}
+mkdir -p "${XDG_STATE_HOME}"
+mkdir -p "${HOME}"/.local/{bin,etc}
+chmod 700 "${XDG_CONFIG_HOME}/gnupg"
+print "  ...done"
+
+# Link zshenv if needed
+print "Checking for ZDOTDIR env variable..."
+if [[ "${ZDOTDIR}" = "${SCRIPT_DIR}/zsh" ]]; then
+    print "  ...present and valid, skipping .zshenv symlink"
+else
+    ln -sf "${SCRIPT_DIR}/zsh/.zshenv" "${ZDOTDIR:-${HOME}}/.zshenv"
+    print "  ...failed to match this script dir, symlinking .zshenv"
+fi
